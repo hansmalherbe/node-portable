c8325fd2d970c1548056fcd5e43a198349c4acbe
Hans Malherbe <hans.malherbe@gmail.com>
2012-10-08 17:30:26 +0200

    Remove unneeded file

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert')
-	, portable = require('../main')
-
-assert.equal(true, portable.existsSync('./tests/testfile.txt'))
-assert.equal(false, portable.existsSync('./tests/x.txt'))
-
-console.log('load ' + module.filename)
-return
-
